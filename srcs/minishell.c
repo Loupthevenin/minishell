61ee0fd4c33ebe5c4bc1f3b481c43e6ca49f8871
Loup Thevenin <loup.thevenin.1@gmail.com>
2025-01-06 14:14:34 +0100

    start minishell

---
+++ srcs/minishell.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 14:01:37 by ltheveni          #+#    #+#             */
+/*   Updated: 2025/01/06 14:12:50 by ltheveni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	(void)argc;
+	(void)argv;
+	(void)env;
+	ft_printf("Hello world!\n");
+	return (0);
+}
