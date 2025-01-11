539added235b2e07124615d38b583cfc2f477677
Loup Thevenin <loup@mac.home>
2025-01-11 15:58:18 +0100

    upgrade error

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: kleung-t <kleung-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 14:01:37 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/01/11 10:06:40 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/01/11 15:23:51 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,13 @@ static void	init_shell(t_shell *shell, char **envp)
 	shell->env = envp;
 	shell->pid = -1;
 	shell->n_pipes = 0;
+}
+
+static void	clean_shell(t_cmd *cmd, t_shell *shell)
+{
+	free_cmd_node(cmd);
+	free_tab((void **)shell->pipefd, shell->n_pipes, 0);
+	/* free_tab((void **)shell->env, 0, 1); */
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -44,5 +51,6 @@ int	main(int argc, char **argv, char **envp)
 		// exec_cmd(cmd, shell);
 		free(input);
 	}
+	clean_shell(cmd, &shell);
 	return (0);
 }
