c83045a5ca2398e69919fb78bf6687cad8a3a470
Loup Thevenin <loup.thevenin.1@gmail.com>
2025-01-15 15:34:30 +0100

    add struct env

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 14:02:01 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/01/14 09:55:55 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/01/15 14:26:31 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,18 @@ typedef struct s_cmd
 	struct s_cmd	*next;
 }					t_cmd;
 
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}					t_env;
+
+// retirer le char **env;
 typedef struct s_shell
 {
 	char			**env;
+	t_env			*env_list;
 	int				**pipefd;
 	pid_t			pid;
 	int				n_pipes;
@@ -41,12 +50,18 @@ typedef struct s_shell
 }					t_shell;
 
 // utils
+void				free_tab(void **tabs, int size, int is_null);
+void				close_pipes(int **pipes, int n);
+
+// utils cmd
 t_cmd				*create_node(char **args);
 void				append_node(t_cmd **head, t_cmd *new_node);
 int					get_command_count(t_cmd *head);
-void				free_tab(void **tabs, int size, int is_null);
 void				free_cmd_node(t_cmd *head);
-void				close_pipes(int **pipes, int n);
+
+// utils env
+t_env				*double_array_to_list(char **envp);
+char				**list_to_double_array(t_env *env);
 
 // parsing
 int					ft_op(const char *s);
