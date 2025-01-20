e062ed577324ba7add7824dc602be680d594e41b
Loup Thevenin <loup@mac.home>
2025-01-21 00:12:33 +0100

    builtin_exit

--- srcs/exec/exec_builtins.c
+++ srcs/exec/exec_builtins.c
@@ -6,26 +6,45 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 18:07:41 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/01/10 10:08:07 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/01/19 17:24:34 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-/* void	exec_builtins(t_cmd *cmd) */
-/* { */
-/* 	if (!ft_strcmp(cmd->args[0], "echo")) */
-/* 		builtin_echo(cmd->args); */
-/* 	else if (!ft_strcmp(cmd->args[0], "cd")) */
-/* 		builtin_cd(cmd->args); */
-/* 	else if (!ft_strcmp(cmd->args[0], "pwd")) */
-/* 		builtin_pwd(cmd->args); */
-/* 	else if (!ft_strcmp(cmd->args[0], "export")) */
-/* 		builtin_export(cmd->args); */
-/* 	else if (!ft_strcmp(cmd->args[0], "unset")) */
-/* 		builtin_unset(cmd->args); */
-/* 	else if (!ft_strcmp(cmd->args[0], "env")) */
-/* 		builtin_env(cmd->args); */
-/* 	else if (!ft_strcmp(cmd->args[0], "exit")) */
-/* 		builtin_exit(cmd->args); */
-/* } */
+int	is_builtins(t_cmd *cmd)
+{
+	if (!ft_strcmp(cmd->args[0], "echo"))
+		return (1);
+	else if (!ft_strcmp(cmd->args[0], "cd"))
+		return (1);
+	else if (!ft_strcmp(cmd->args[0], "pwd"))
+		return (1);
+	else if (!ft_strcmp(cmd->args[0], "export"))
+		return (1);
+	else if (!ft_strcmp(cmd->args[0], "unset"))
+		return (1);
+	else if (!ft_strcmp(cmd->args[0], "env"))
+		return (1);
+	else if (!ft_strcmp(cmd->args[0], "exit"))
+		return (1);
+	return (0);
+}
+
+void	exec_builtins(t_shell *shell, t_cmd *cmd)
+{
+	if (!ft_strcmp(cmd->args[0], "echo"))
+		builtin_echo(shell, cmd);
+	else if (!ft_strcmp(cmd->args[0], "cd"))
+		builtin_cd(shell, cmd);
+	else if (!ft_strcmp(cmd->args[0], "pwd"))
+		builtin_pwd(shell, cmd);
+	else if (!ft_strcmp(cmd->args[0], "export"))
+		builtin_export(shell, cmd);
+	else if (!ft_strcmp(cmd->args[0], "unset"))
+		builtin_unset(shell, cmd);
+	else if (!ft_strcmp(cmd->args[0], "env"))
+		builtin_env(shell, cmd);
+	else if (!ft_strcmp(cmd->args[0], "exit"))
+		builtin_exit(shell, cmd);
+}
--- srcs/exec/fork_processes.c
+++ srcs/exec/fork_processes.c
@@ -6,30 +6,11 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 11:38:01 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/01/16 17:36:10 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/01/19 17:03:41 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-/* static int	is_builtins(t_cmd *cmd) */
-/* { */
-/* 	if (!ft_strcmp(cmd->args[0], "echo")) */
-/* 		return (1); */
-/* 	else if (!ft_strcmp(cmd->args[0], "cd")) */
-/* 		return (1); */
-/* 	else if (!ft_strcmp(cmd->args[0], "pwd")) */
-/* 		return (1); */
-/* 	else if (!ft_strcmp(cmd->args[0], "export")) */
-/* 		return (1); */
-/* 	else if (!ft_strcmp(cmd->args[0], "unset")) */
-/* 		return (1); */
-/* 	else if (!ft_strcmp(cmd->args[0], "env")) */
-/* 		return (1); */
-/* 	else if (!ft_strcmp(cmd->args[0], "exit")) */
-/* 		return (1); */
-/* 	return (0); */
-/* } */
 
 static void	handle_fork_error(t_cmd *cmd, t_shell *shell)
 {
