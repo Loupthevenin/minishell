a18c31abe6ebc1bcd67257d861ad88d241a6372c
Loup Thevenin <loup.thevenin.1@gmail.com>
2025-01-21 11:50:52 +0100

    builtin_unset

--- srcs/builtins/builtin_env.c
+++ srcs/builtins/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 15:47:24 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/01/21 10:38:19 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/01/21 10:56:19 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,5 @@ void	builtin_env(t_shell *shell, t_cmd *cmd)
 			printf("%s=%s\n", current->key, current->value);
 		current = current->next;
 	}
+	shell->last_exit = 0;
 }
--- srcs/builtins/builtin_unset.c
+++ srcs/builtins/builtin_unset.c
@@ -6,14 +6,77 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 15:49:38 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/01/19 15:50:09 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/01/21 11:45:43 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+static int	check_identifier(char *s)
+{
+	int	i;
+
+	if (!s || (!ft_isalpha(s[0]) && s[0] != '_'))
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (!ft_isalpha(s[i]) && s[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static void	unset_env_var(t_env **env, const char *key)
+{
+	t_env	*current;
+	t_env	*prev;
+
+	if (!env || !*env || !key)
+		return ;
+	current = *env;
+	prev = NULL;
+	while (current)
+	{
+		if (!ft_strcmp(current->key, key))
+		{
+			if (!prev)
+				*env = current->next;
+			else
+				prev->next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			return ;
+		}
+		prev = current;
+		current = current->next;
+	}
+}
+
 void	builtin_unset(t_shell *shell, t_cmd *cmd)
 {
-	(void)shell;
-	(void)cmd;
+	int	i;
+
+	if (!cmd->args[1])
+	{
+		shell->last_exit = 0;
+		return ;
+	}
+	i = 1;
+	while (cmd->args[i])
+	{
+		if (!check_identifier(cmd->args[i]))
+		{
+			ft_putstr_fd("minishell: unset: `", 2);
+			ft_putstr_fd(cmd->args[i], 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
+			shell->last_exit = EXIT_FAILURE;
+		}
+		else
+			unset_env_var(&shell->env_list, cmd->args[i]);
+		i++;
+	}
+	shell->last_exit = 0;
 }
