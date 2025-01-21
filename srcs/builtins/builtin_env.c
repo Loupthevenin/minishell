ac98d8e1f767f0280a183139118ba3a71f278185
Loup Thevenin <loup.thevenin.1@gmail.com>
2025-01-21 10:38:49 +0100

    builtin_env

--- srcs/builtins/builtin_env.c
+++ srcs/builtins/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 15:47:24 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/01/19 15:47:55 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/01/21 10:38:19 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 void	builtin_env(t_shell *shell, t_cmd *cmd)
 {
-	(void)shell;
+	t_env	*current;
+
 	(void)cmd;
+	current = shell->env_list;
+	while (current)
+	{
+		if (current->key && current->value)
+			printf("%s=%s\n", current->key, current->value);
+		current = current->next;
+	}
 }
