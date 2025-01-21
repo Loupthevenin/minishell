6d7ef7c802f1e59376f7aaa843cb343ec9359ca1
Loup Thevenin <loup@mac.home>
2025-01-21 10:05:18 +0100

    builtin_echo

--- srcs/builtins/builtin_echo.c
+++ srcs/builtins/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 15:48:48 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/01/19 15:49:20 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/01/21 10:04:17 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 
 void	builtin_echo(t_shell *shell, t_cmd *cmd)
 {
+	int	i;
+	int	newline;
+
 	(void)shell;
-	(void)cmd;
+	newline = 1;
+	i = 1;
+	if (!ft_strcmp(cmd->args[1], "-n"))
+	{
+		newline = 0;
+		i++;
+	}
+	while (cmd->args[i])
+	{
+		ft_putstr_fd(cmd->args[i], 1);
+		ft_putchar_fd(' ', 1);
+		i++;
+	}
+	if (newline)
+		ft_putchar_fd('\n', 1);
 }
