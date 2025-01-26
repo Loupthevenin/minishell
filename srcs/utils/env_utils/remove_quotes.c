c47711611ef935947c03f5523481543035a1ae18
Loup Thevenin <loup.thevenin.1@gmail.com>
2025-01-26 19:37:16 +0100

    add single/double on spaces_operator, remove_quotes

--- srcs/utils/env_utils/remove_quotes.c
+++ srcs/utils/env_utils/remove_quotes.c
@@ -6,7 +6,7 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 15:56:18 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/01/26 16:25:03 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/01/26 18:46:21 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,44 @@ char	*remove_s_quotes(char *s)
 	return (result);
 }
 
+static int	check_quotes(char *arg, int *in_single_quote, int *in_double_quote,
+		int *k)
+{
+	if (arg[*k] == '\'' && !*in_double_quote)
+	{
+		*in_single_quote = !*in_single_quote;
+		(*k)++;
+		return (1);
+	}
+	else if (arg[*k] == '"' && !*in_single_quote)
+	{
+		*in_double_quote = !*in_double_quote;
+		(*k)++;
+		return (1);
+	}
+	return (0);
+}
+
+static void	second_loop(char *arg, char *new_arg, int len)
+{
+	int	j;
+	int	k;
+	int	in_single_quote;
+	int	in_double_quote;
+
+	j = 0;
+	k = 0;
+	in_single_quote = 0;
+	in_double_quote = 0;
+	while (k < len)
+	{
+		if (check_quotes(arg, &in_single_quote, &in_double_quote, &k))
+			continue ;
+		new_arg[j++] = arg[k++];
+	}
+	new_arg[j] = '\0';
+}
+
 void	remove_quotes(t_cmd **cmd)
 {
 	char	**args;
@@ -41,16 +79,13 @@ void	remove_quotes(t_cmd **cmd)
 	i = 0;
 	while (args[i])
 	{
-		if (args[i][0] == '"' && args[i][ft_strlen(args[i]) - 1] == '"')
-		{
-			len = ft_strlen(args[i]);
-			new_arg = ft_substr(args[i], 1, len - 2);
-			if (new_arg)
-			{
-				free(args[i]);
-				args[i] = new_arg;
-			}
-		}
+		len = ft_strlen(args[i]);
+		new_arg = malloc(sizeof(char) * (len + 1));
+		if (!new_arg)
+			return ;
+		second_loop(args[i], new_arg, len);
+		free(args[i]);
+		args[i] = new_arg;
 		i++;
 	}
 }
