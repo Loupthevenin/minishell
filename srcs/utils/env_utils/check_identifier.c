0d111ea3939fcfb9c292988acdb7e4a369e0acea
Loup Thevenin <loup@MacBook-Pro-de-Loup.local>
2025-01-27 12:22:14 +0100

    fix all builtins

--- srcs/utils/env_utils/check_identifier.c
+++ srcs/utils/env_utils/check_identifier.c
@@ -6,7 +6,7 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 19:26:28 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/01/21 19:27:18 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/01/27 11:55:52 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,14 @@ int	check_identifier_export(char *s)
 {
 	int	i;
 
-	if (!s || (!ft_isalpha(s[0]) && s[0] != '_'))
+	if (!s || s[0] == '\0')
+		return (0);
+	if (!ft_isalpha(s[0]) && s[0] != '_')
 		return (0);
 	i = 1;
 	while (s[i] && s[i] != '=')
 	{
-		if (!ft_isalpha(s[i]) && s[i] != '_')
+		if (!ft_isalnum(s[i]) && s[i] != '_')
 			return (0);
 		i++;
 	}
